27006805ed65d5067524b28e73a69c609819bb48
JoKing <joebryant614@gmail.com>
2021-05-31 11:45:57 -0700

    add emp works

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function trackEmp() {
                     break;
 
                 case 'Add an employee':
-                    console.log('hi')
+                    addEmp()
                     break;
 
                 case 'Update an employee role':
@@ -126,7 +126,7 @@ function addDep() {
 }
 
 
-//! add demp role function 
+//! add emp role function 
 function addEmpRole() {
     db.query(`SELECT * FROM department`, (err, rows) => {
         // we want access to the information in this table so we will run inquirer in this call back function 
@@ -172,15 +172,15 @@ function addEmpRole() {
                 const department = answers.department;
                 let depId;
                 // FOR/OF LOOP: loops through the values of an iterable array 
-                for(const row of rows){
-                    if(row.dep_name === department){
+                for (const row of rows) {
+                    if (row.dep_name === department) {
                         depId = row.id;
                     }
                 }
-                
+
                 //answers.salary is a sting we need to turn it into a number
-                const salary = parseInt(answers.salary) 
-                
+                const salary = parseInt(answers.salary)
+
                 const sql = `INSERT INTO emp_role (title, salary, department_id) VALUES (?,?,?);`
                 const params = [answers.title, salary, depId]
 
@@ -190,10 +190,97 @@ function addEmpRole() {
                     trackEmp();
                 })
             });
-
-
     });
+}
 
 
+//! add an emp
+function addEmp() {
+    db.query(`SELECT * FROM emp_role`, (err, rows) => {
+        if (err) throw err;
+        const activeRoles = rows.map(x => x.title);
 
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is the employee's first name?",
+                    name: "firstName",
+                    validate: nameInput => {
+                        if (nameInput) {
+                            return true;
+                        } else {
+                            console.log('You must Answer!!!');
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: "input",
+                    message: "What is the employee's last name?",
+                    name: "lastName",
+                    validate: nameInput => {
+                        if (nameInput) {
+                            return true;
+                        } else {
+                            console.log('You must Answer!!!');
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: "list",
+                    message: "What is the employees role?",
+                    name: "role",
+                    choices: activeRoles
+                }
+            ]).then(answers => {
+                const role = answers.role;
+                let roleId;
+                // FOR/OF LOOP: loops through the values of an iterable array 
+                for (const row of rows) {
+                    if (row.title === role) {
+                        roleId = row.id;
+                    }
+                }
+
+                //another query call to get active employee list 
+                db.query(`SELECT * FROM employee`, (err2, rows2) => {
+                    if (err2) throw err2;
+                    const activeEmps = rows2.map(x => x.first_name);
+                    activeEmps.push("No");
+
+                    inquirer
+                        .prompt(
+                            {
+                                type: "list",
+                                message: "Does this employee's have a manager?",
+                                name: "manager",
+                                choices: activeEmps,
+                            }).then(answer => {
+                                const manager = answer.manager;
+
+                                let managerID;
+                                if (manager === 'No') {
+                                    managerID = 'NULL';
+                                }
+
+                                for (const row2 of rows2) {
+                                    if (row2.first_name === manager) {
+                                        managerID = row2.id;
+                                    }
+                                }
+
+                                const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?);`
+                                const params = [answers.firstName, answers.lastName, roleId, managerID]
+                                
+                                db.query(sql, params, (err3, rows3) => {
+                                    if (err3) throw err3;
+                                    console.log('Success');
+                                    trackEmp();
+                                });
+                            });
+                });
+            });
+    });
 }
